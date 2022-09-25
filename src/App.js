247dbcdfcbdd3b59539d60fe71b57d5d997cc267
Esila-A <esila.adajewa@gmail.com>
2022-09-25 04:31:30 +0200

    path try

--- src/App.js
+++ src/App.js
@@ -13,11 +13,11 @@ function App() {
     <div className="app">
       <Navigation />
       <Routes>
-        <Route path="oddam-rzeczy/" element={<Home />} />
-        <Route path="oddam-rzeczy/sign-in" element={<SignIn />} />
-        <Route path="oddam-rzeczy/sign-up" element={<SignUp />} />
-        <Route path="oddam-rzeczy/sign-out" element={<SignOut />} />
-        <Route path="oddam-rzeczy/hand-over" element={<HandOver />} />
+        <Route path="oddam-rzeczy-project/" element={<Home />} />
+        <Route path="oddam-rzeczy-project/sign-in" element={<SignIn />} />
+        <Route path="oddam-rzeczy-project/sign-up" element={<SignUp />} />
+        <Route path="oddam-rzeczy-project/sign-out" element={<SignOut />} />
+        <Route path="oddam-rzeczy-project/hand-over" element={<HandOver />} />
       </Routes>
     </div>
   );
